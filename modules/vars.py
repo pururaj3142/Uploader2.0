e6b6c0aa8eb8195b46829dda438d80e1e2959bb3
pururaj3142 <ppururaj@gmail.com>
2025-11-24 17:11:57 +0530

    Update API credentials and owner details

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,20 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "22049628"))
+API_HASH = environ.get("API_HASH", "db9c4ee4be741f296522a9bb9a945722")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7830622844:AAGNtAcsGgO5DemDvcq_mwWj8r4E6P9Y6Kk")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "670897324"))
+CREDIT = environ.get("CREDIT", "DP BOTS")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '670897324').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '670897324').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
+
